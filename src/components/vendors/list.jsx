2ad5e43d9121144c33b675ae6cc53ee16b386881
Marcelo Salazar R <chelosalazar@gmail.com>
2015-09-24 01:42:39 -0300

    Removed material-ui and materialize-css
    Signed-off-by: Marcelo Salazar R <chelosalazar@gmail.com>

--- src/components/vendors/list.jsx
+++ src/components/vendors/list.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import { TextField } from 'material-ui'
-let ThemeManager = new (require('material-ui/lib/styles/theme-manager'))()
 
 const List = React.createClass({
-  childContextTypes: {
-    muiTheme: React.PropTypes.object
-  },
-  getChildContext: function() {
-    return {
-      muiTheme: ThemeManager.getCurrentTheme()
-    };
-  },
   render() {
     return (<form><h3>List</h3>
-    <TextField hintText="Hint Text" floatingLabelText="My awsome company name" /><br />
-    <TextField hintText="Password Field" floatingLabelText="Use a strong pass" type="password" /><br />
+    <div>My awsome company name</div><br />
+    <input type="password" /><br />
     </form>)
   }
 })
